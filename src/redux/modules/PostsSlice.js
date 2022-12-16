30b45b2d5c602703f8e487d993910477b3090478
yujin <sonewdim@gmail.com>
2022-12-16 21:56:32 +0900

    init onpiece client

---
+++ src/redux/modules/PostsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: [
+    {
+      userId: 1,
+      nickname: "",
+      title: "",
+      content: "",
+      category: [],
+      level: "",
+      headCount: 1,
+      recruitmentEndDay: 1671178923,
+      studyStartTime: "13:00:00",
+      studyEndTime: "14:00:00",
+      studyStartDay: "2021-02-21",
+      studyEndDay: "2021-02-21",
+      applicants: [],
+    },
+  ],
+  isLoading: false,
+};
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    getPosts: (state, payload) => {
+      console.log(state.posts, payload);
+    },
+  },
+});
+
+export default postsSlice.reducer;
